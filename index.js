8739a9c245d86463167a54aefcde998d90434ac9
Tiana Lopez <tianadavin@gmail.com>
2023-08-08 10:09:22 -0700

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const newCatsEnd = [...cats, name];
+  return newCatsEnd;
+}
+
+function prependCat(name) {
+  const newCatsBegin = [name, ...cats];
+  return newCatsBegin;
+}
+
+function removeLastCat() {
+  const lessCats = cats.slice(0, cats.length -1);
+  return lessCats;
+}
+
+function removeFirstCat() {
+  const firstLessCats = cats.slice(1);
+  return firstLessCats;
+}
